e55de25f3f5669b5560539762f5e685065efd9b0
Ekweaga <hikay133@gmail.com>
2022-12-12 22:53:10 +0100

    spacing

--- components/search.jsx
+++ components/search.jsx
@@ -6,7 +6,7 @@ function Search({loading,data,error}) {
 {
    error !== null ? <div className='flex items-center justify-center'>{error}</div>:(<> <div>
     {
-      loading ? <div className="flex items-center justify-center text-3xl font-bold">Loading.....</div> : <div className='grid grid-cols-2 md:grid-cols-4'>{
+      loading ? <div className="flex items-center justify-center text-3xl font-bold">Loading.....</div> : <div className='grid grid-cols-2 md:grid-cols-4 gap-[20px]'>{
         data.map((item)=>{
           return (<img src={item?.backdrop_path? `http://image.tmdb.org/t/p/w500/${item?.backdrop_path}`: "/Rectangle 5.png"} alt={item?.title} className="w-full h-full block"/>)
         })} </div>
